virt-lightning/virt-lightning

commit 4e7aa021a7f3d1f2ec8a0f44a35620bcea2d3f87

vl down: no failure if ipv4 metadata missing
The ipv4 key may be missing. Handle this case.

virt_lightning/virt_lightning.py
@@ -299,10 +299,13 @@ class LibvirtHypervisor:
         )
 
     def clean_up(self, domain):
-        self.dns_entry(
-            libvirt.VIR_NETWORK_UPDATE_COMMAND_DELETE, domain.name, domain.ipv4
-        )
-        self.dhcp_entry(libvirt.VIR_NETWORK_UPDATE_COMMAND_DELETE, domain.ipv4)
+        if domain.ipv4:
+            self.dns_entry(
+                libvirt.VIR_NETWORK_UPDATE_COMMAND_DELETE,
+                domain.name, domain.ipv4
+            )
+            self.dhcp_entry(libvirt.VIR_NETWORK_UPDATE_COMMAND_DELETE,
+                            domain.ipv4)
         xml = domain.dom.XMLDesc(0)
         state, _ = domain.dom.state()
         if state != libvirt.VIR_DOMAIN_SHUTOFF:
@@ -641,7 +644,8 @@ class LibvirtDomain:
 
     @property
     def ipv4(self):
-        return ipaddress.IPv4Interface(self.get_metadata("ipv4"))
+        if self.get_metadata("ipv4"):
+            return ipaddress.IPv4Interface(self.get_metadata("ipv4"))
 
     @ipv4.setter
     def ipv4(self, value):

commit 8e9e1747545feb7cd4cfc981adf3dadc51f8b5f2

black: refresh the indentation
Fix an incorrect indentation introduced in
4e7aa021a7f3d1f2ec8a0f44a35620bcea2d3f87.

virt_lightning/virt_lightning.py
@@ -301,11 +301,9 @@ class LibvirtHypervisor:
     def clean_up(self, domain):
         if domain.ipv4:
             self.dns_entry(
-                libvirt.VIR_NETWORK_UPDATE_COMMAND_DELETE,
-                domain.name, domain.ipv4
+                libvirt.VIR_NETWORK_UPDATE_COMMAND_DELETE, domain.name, domain.ipv4
             )
-            self.dhcp_entry(libvirt.VIR_NETWORK_UPDATE_COMMAND_DELETE,
-                            domain.ipv4)
+            self.dhcp_entry(libvirt.VIR_NETWORK_UPDATE_COMMAND_DELETE, domain.ipv4)
         xml = domain.dom.XMLDesc(0)
         state, _ = domain.dom.state()
         if state != libvirt.VIR_DOMAIN_SHUTOFF: