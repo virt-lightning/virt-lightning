a36f9d6e71bab8e5b152f135decc07cf77af0998
jpic <jpic@yourlabs.org>
2022-03-19 08:41:59 +0100

    Add remaining commands

--- virt_lightning/shell_cli2.py
+++ virt_lightning/shell_cli2.py
@@ -5,8 +5,7 @@
 import cli2
 import inspect
 import sys
-from virt_lightning import api
-from virt_lightning.shell import how_to_fix_auth_error
+from virt_lightning import api, shell, ui
 from virt_lightning.configuration import Configuration
 
 
@@ -35,7 +34,7 @@ class Command(cli2.Command):
             print(f'Image not found from url: {e.name}')  # noqa: T001
             sys.exit(1)
         except api.CannotConnectToLibvirtError:
-            how_to_fix_auth_error()
+            shell.how_to_fix_auth_error()
         except api.VMNotRunningError as e:
             print(f'The following instance is not running: {e.name}')  # noqa: T001
             sys.exit(1)
@@ -119,3 +118,111 @@ def create(distro, name=None, vcpus=None, memory=None, context='default',
         distro=distro,
         enable_console=console,
     )
+
+
+@cli.cmd(color='green')
+def distro_list():
+    """
+    List distributions supported by Virt-Lightning.
+    """
+    for distro_name in api.distro_list(configuration=configuration):
+        print("- distro: {0}".format(distro_name))  # noqa: T001
+
+
+@cli.cmd(color='green')
+def storage_dir():
+    """
+    Dump storage directory path for Virt-Lightning VMs.
+    """
+    print(api.storage_dir(configuration=configuration))
+
+
+@cli.cmd(color='green')
+def ansible_inventory():
+    """
+    Dump ansible inventory file.
+
+    Example:
+
+        vl2 ansible_inventory > inventory.ini
+        ansible -m ping -i inventory.ini all
+    """
+    print(api.ansible_inventory(configuration=configuration))
+
+
+@cli.cmd(color='green')
+def ssh_config(context='default'):
+    """
+    Dump ssh configuration for the VMs.
+    """
+    print(api.ssh_config(configuration=configuration, context=context))
+
+
+@cli.cmd(color='green')
+def ssh(name=None, context='default'):
+    """
+    Open an SSH connection on a VM.
+
+    :param name: VM name, if not specified then VM selector UI will ask.
+    """
+    if name:
+        api.exec_ssh(configuration=configuration, name=name)
+
+    def go_ssh(domain):
+        domain.exec_ssh()
+
+    ui.Selector(
+        api.list_domains(configuration=configuration),
+        go_ssh,
+    )
+
+
+@cli.cmd(color='green')
+def fetch(distro):
+    """
+    Fetch a distribution image.
+
+    :param distro: Name of the distribution for the VM image to download.
+    """
+    def progress_callback(cur, length):
+        percent = (cur * 100) / length
+        line = "🌍 ➡️  💻 [{percent:06.2f}%]  {done:6}MB/{full}MB\r".format(
+            percent=percent,
+            done=int(cur / api.MB),
+            full=int(length / api.MB),
+        )
+        print(line, end="")  # noqa: T001
+
+    try:
+        api.fetch(
+            configuration=configuration,
+            progress_callback=progress_callback,
+            distro=distro,
+        )
+    except api.ImageNotFoundUpstreamError:
+        print(  # noqa: T001
+            f"Distro {distro} cannot be downloaded.\n"
+            f"  Visit {api.BASE_URL}/images/ or private image hub"
+            "to get an up to date list."
+        )
+        exit(1)
+
+
+@cli.cmd(color='green')
+def console(name):
+    """
+    Open a serial console to a named VM.
+
+    :param name: VM name
+    """
+    shell.console(configuration=configuration, name=name)
+
+
+@cli.cmd(color='green')
+def viewer(name):
+    """
+    Open a graphical screen to a named VM.
+
+    :param name: VM name
+    """
+    shell.viewer(configuration=configuration, name=name)
