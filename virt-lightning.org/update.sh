fe8816ac40e1071b9693cb20090950e61ab9e650
Gonéri Le Bouder <goneri@lebouder.net>
2024-09-09 08:43:02 -0400

    virt-lightning.org: also update the README.md

--- virt-lightning.org/update.sh
+++ virt-lightning.org/update.sh
@@ -2,4 +2,5 @@
 
 scp etc/caddy/conf.d/virt-lightning.org.conf virt-lightning.org:/etc/caddy/conf.d/virt-lightning.org.conf
 scp www/images/index.md root@virt-lightning.org:/var/www/virt-lightning.org/images
+scp ../README.md root@virt-lightning.org:/var/www/virt-lightning.org/index.md
 ssh virt-lightning.org sudo systemctl restart caddy
