cfb3f15f6fd015fdc5c7cce71001af99a58413c5
Gonéri Le Bouder <goneri@lebouder.net>
2024-11-06 18:46:30 -0500

    add images.json and refresh the image lists
    The commit introduces a new file called `images.json` that will later
    replace the Caddy webserver. We will modify virt-lightning fetch in
    order to retrieve this `images.json` and avoid the complex system of HTTP
    redirection that we have today.
    
    This commits also remove a series of outdated images (`404`).
    
    Also, import `refresh.py` is a help script used to maintain the different files.

--- virt-lightning.org/update.sh
+++ virt-lightning.org/update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+python3 refresh.py
 scp etc/caddy/conf.d/virt-lightning.org.conf virt-lightning.org:/etc/caddy/conf.d/virt-lightning.org.conf
 scp www/images/index.md root@virt-lightning.org:/var/www/virt-lightning.org/images
 scp ../README.md root@virt-lightning.org:/var/www/virt-lightning.org/index.md
